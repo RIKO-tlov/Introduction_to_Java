40cb1716a896b2c857ff346e3d26cfe69aa64309
RIKO-tlov <bluebyrair@outlook.jp>
2021-07-27 11:29:08 +0900

    [finish]Chapter1 quiz

---
+++ Chapter1/Main4.java
@@ -0,0 +1,20 @@
+/* 1-1 (ア)コンパイラ　(イ)インタプリタ (ウ)ソースコード
+       (エ)バイトコード (オ)JVM */
+
+//1-2
+public class Main4 {
+  public static void main (String[] args) {
+    int a = 3;
+    int b = 5;
+    int c = a * b;
+    System.out.println("縦幅3横幅5の長方形の面積は、" + c);
+  }
+}
+
+/*1-3
+①　boolean result = true;
+②　char capital = '駆';
+③  double pie = 3.14;
+④  long number = 314159265853979L;
+⑤  String sentence = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+*/
