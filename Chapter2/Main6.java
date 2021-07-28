bc68114861b3cf305d6c7b26b1de32ca1ecffd07
RIKO-tlov <bluebyrair@outlook.jp>
2021-07-28 10:09:42 +0900

    [add]Chapter2 改行なし文字表示

---
+++ Chapter2/Main6.java
@@ -0,0 +1,10 @@
+//改行なし画面出力の命令
+
+public class Main6 {
+  public static void main(String[] args) {
+    String name = "生田";
+    System.out.print("私の名前は");
+    System.out.print(name);
+    System.out.print("です。");
+  }
+}
