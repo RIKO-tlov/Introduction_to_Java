4593d2a4e0d0f2474453025ecedd1881dd31e879
RIKO-tlov <bluebyrair@outlook.jp>
2021-07-28 23:45:53 +0900

    [add]Chapter3 for文

---
+++ Chapter3/Main4.java
@@ -0,0 +1,19 @@
+/* 基本形
+public class Main4 {
+  public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+      System.out.println("こんにちは");
+    }
+  }
+}
+*/
+
+public class Main4 {
+  public static void main(String[] args) {
+    for (int i = 0; i < 3; i++) {
+      System.out.print("現在" + (i + 1) + "週目→");
+    }
+  }
+}
+
+
