b8701f9a8564a1fd9e9f84fdda70acb740d80c78
RIKO-tlov <bluebyrair@outlook.jp>
2021-07-30 13:50:45 +0900

    [finish]Chapter6 quiz

---
+++ Chapter6/Main.java
@@ -0,0 +1,20 @@
+//6-1 6-2
+import comment.Zenhan;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    Zenhan.doWarusa();
+    Zenhan.doTogame();
+    comment.Kouhan.callDeae();
+    comment.Kouhan.showMondokoro();
+  }
+}
+
+/*6-4
+c:\work\ex64
+c:\work\ex64\comment
+*/
+
+//6-5 c:\javaapp\koumon
+
+
