8f5c76ba4dca0df87f17ed34998d81786b4acb3b
RIKO-tlov <bluebyrair@outlook.jp>
2021-08-02 13:38:32 +0900

    [add]Chapter9　コンストラクタ

--- Chapter9/Hero.java
+++ Chapter9/Hero.java
@@ -6,4 +6,11 @@ public class Hero {
     System.out.println(this.name + "は" + sword.name + "で攻撃した！");
     System.out.println("敵に5ポイントのダメージをあたえた！");
   }
+  public Hero(String name) { //コンストラクタ①
+    this.hp = 100;  //hpフィールドを100で初期化
+    this.name = name; //引数の値でnameフィールドを初期化
+  }
+  public Hero() { //コンストラクタ②
+    this("ダミー"); //コンストラクタ①を呼び出すようJVMに依頼する
+  }
 }
--- Chapter9/Main.java
+++ Chapter9/Main.java
@@ -3,11 +3,20 @@ public class Main {
     Sword s = new Sword();
     s.name = "炎の剣";
     s.damage = 10;
-    Hero h = new Hero();
-    h.name = "ミナト";
-    h.hp = 100;
-    h.sword = s;
-    System.out.println("現在の武器は" + h.sword.name);
-    h.attack();
+    Hero h1 = new Hero("ミナト");
+    System.out.println(h1.hp);
+    System.out.println(h1.name);
+    h1.sword = s;
+    System.out.println("現在の武器は" + h1.sword.name);
+    h1.attack();
+
+    Hero h2 = new Hero();
+    System.out.println(h2.hp);
+    Wizard w = new Wizard();
+    w.name = "スガワラ";
+    w.hp = 50;
+    w.heal(h1);
+    w.heal(h2);
+    w.heal(h2);
   }
 }
