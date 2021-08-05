eb0ae890fe7aff8baf0aa2617641e7fc174203d5
RIKO-tlov <bluebyrair@outlook.jp>
2021-08-05 20:18:08 +0900

    [finish]Chapter11 quiz

--- Chapter11/TangibleAsset.java
+++ Chapter11/TangibleAsset.java
@@ -1,13 +1,11 @@
-public abstract class TangibleAsset {
-  String name;
-  int price;
+public abstract class TangibleAsset extends Assets implements Thing {
   String color;
+  double weight;
   public TangibleAsset(String name, int price, String color) {
-    this.name = name;
-    this.price = price;
+    super(name, price);
     this.color = color;
   }
-  public String getName() { return this.name; }
-  public int getPrice() { return this.price; }
   public String getColor() { return this.Color; }
+  public double getWeight() { return this.weight; }
+  public void setWeight(double weight) { this.weight = weight; }
 }
