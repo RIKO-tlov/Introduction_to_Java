ec3166a931f17d8766806278466eb81ae6eac960
RIKO-tlov <bluebyrair@outlook.jp>
2021-08-08 17:01:50 +0900

    [finish]Chapter13 quiz

--- Chapter13/Wizard.java
+++ Chapter13/Wizard.java
@@ -8,17 +8,34 @@ public class Wizard {
   public void heal(Hero h) {
     int basePoint = 10;                                             //基本回復ポイント
     int recovPoint = (int)(basePoint * this.getWand().gePower());   //杖による増幅
-    h.setHp(h.getHp() + recovPoint);                     //勇者のHPを回復する
+    h.setHp(h.getHp() + recovPoint);                     　　　　　 //勇者のHPを回復する
     System.out.println(h.getName() + "のHPを" + recovPoint + "回復した！");
   }
   public int getHp() { return this.hp; }
-  public void setHp(int hp) { this.hp = hp; }
+  public void setHp(int hp) {
+    if (hp < 0) { this.hp = 0; } else { this.hp = hp; }
+  }
   public int getMp() { return this.mp; }
-  public void setMp(int mp) { this.mp = mp; }
+  public void setMp(int mp) {
+    if (mp < 0) {
+      throw new illegalArgumentException ("設定されようとしているMPが異常です");
+    }
+    this.mp = mp;
+  }
   public String getName() { return this.name; }
-  public void setName(String name) { this.name = name; }
+  public void setName(String name) {
+    if (name == null || name.length() < 3) {
+      throw new illegalArgumentException ("魔法使いに設定されようとしている名前が異常です");
+    }
+    this.name = name;
+  }
   public Wand getWand() { return this.wand; }
-  public void setWand(Wand wand) { this.wand = wand; }
+  public void setWand(Wand wand) {
+    if (wand == null) {
+      throw new illegalArgumentException ("設定されようとしている杖がnullです");
+    }
+    this.wand = wand;
+  }
 }
 
 /*13-2
