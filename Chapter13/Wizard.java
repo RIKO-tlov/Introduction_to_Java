e518a3d23c3b65193f8e3756c2fdcc5064505d60
RIKO-tlov <bluebyrair@outlook.jp>
2021-08-08 16:33:48 +0900

    [start]Chapter13 quiz

---
+++ Chapter13/Wizard.java
@@ -0,0 +1,28 @@
+//13-1
+
+public class Wizard {
+  private int hp;
+  private int mp;
+  private String name;
+  private Wand wand;
+  public void heal(Hero h) {
+    int basePoint = 10;                                             //基本回復ポイント
+    int recovPoint = (int)(basePoint * this.getWand().gePower());   //杖による増幅
+    h.setHp(h.getHp() + recovPoint);                     //勇者のHPを回復する
+    System.out.println(h.getName() + "のHPを" + recovPoint + "回復した！");
+  }
+  public int getHp() { return this.hp; }
+  public void setHp(int hp) { this.hp = hp; }
+  public int getMp() { return this.mp; }
+  public void setMp(int mp) { this.mp = mp; }
+  public String getName() { return this.name; }
+  public void setName(String name) { this.name = name; }
+  public Wand getWand() { return this.wand; }
+  public void setWand(Wand wand) { this.wand = wand; }
+}
+
+/*13-2
+内容：Wizardクラスで「powerはWandでprivateアクセスされます」
+原因：Wandクラスのpowerフィールドはprivate修飾されているので、Wizardクラスから直接アクセスできないため。
+*/
+
