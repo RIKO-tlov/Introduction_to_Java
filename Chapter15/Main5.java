eb5b70f6ea530da93d808627c193994e35914e2e
RIKO-tlov <bluebyrair@outlook.jp>
2021-08-09 21:58:11 +0900

    [add]Chapter15 regular expression

---
+++ Chapter15/Main5.java
@@ -0,0 +1,9 @@
+public class Main5 {
+  public static void main(String[] args) {
+    String s = "abc,def:ghi";
+    String[] words = s.split("[,:]"); //正規表現パターン
+    for (String w : words) {
+      System.out.println(w + "->");
+    }
+  }
+}
