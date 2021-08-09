b949780adf8ee7f1ba720ddc51105a51b64272a1
RIKO-tlov <bluebyrair@outlook.jp>
2021-08-09 18:00:14 +0900

    [finish]Chapter14 quiz

---
+++ Chapter14/Account.java
@@ -0,0 +1,29 @@
+//14-1
+
+public class Account {
+  String accountNumber;
+  int balance;
+
+  public String toString() {
+    return "¥¥" + this.balance + "(口座番号：" + this.accountNumber + ")";
+  }
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o instanceof Account) {
+      Account a = (Account)o;
+      String an1 = this.accountNumber.trim();
+      String an2 = a.accountNumber.trim();
+      if (an1.equals(an2)) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+/*14-2
+static final int MAX_HP = 50;
+static final int MAX_MP = 10;
+*/
