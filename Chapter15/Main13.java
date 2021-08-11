155a1cea50a0d0ebdb9ea544522da8458c379f8f
RIKO-tlov <bluebyrair@outlook.jp>
2021-08-11 23:10:12 +0900

    [add]Chapter15 quiz

---
+++ Chapter15/Main13.java
@@ -0,0 +1,11 @@
+//15-1
+public class Main13 {
+  public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < 100; i++) {
+      sb.append(i + l).append(",");
+    }
+    String s = sb.toString();
+    String[] a = s.split(",");
+  }
+}
