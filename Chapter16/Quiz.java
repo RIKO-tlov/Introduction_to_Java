fac15e332e6cf876b0e4bf723bd173ffcc35cb39
RIKO-tlov <bluebyrair@outlook.jp>
2021-08-14 15:10:45 +0900

    [finish]Chapter16 quiz

---
+++ Chapter16/Quiz.java
@@ -0,0 +1,29 @@
+/*16-1
+(1)Set
+(2)List
+(3)Map
+*/
+
+/*16-2
+Hero h1 = new Hero("斎藤");
+Hero h2 = new Hero("鈴木");
+List<Hero> heroes = new ArrayList<Hero>();
+Heroes.add(h1);
+Heroes.add(h2);
+for (Hero h : heroes) {
+  System.out.println(h.getName());
+}
+*/
+
+/*16-3
+Hero h1 = new Hero("斎藤");
+Hero h2 = new Hero("鈴木");
+Map<Hero, Integer> heroes = new HashMap<Hero, Integer>();
+heroes.put(h1, 3);
+heroes.put(h2, 7);
+for (Hero key : heroes.keySet()) {
+  int value = heroes.get(key);
+  System.out.println(key.getName() + "が倒した敵＝" + value);
+}
+*/
+
