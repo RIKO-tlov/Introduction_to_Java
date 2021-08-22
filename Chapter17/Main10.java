78b2de2053de3d983bf67f000f84c51a3e76a295
RIKO-tlov <bluebyrair@outlook.jp>
2021-08-22 11:47:38 +0900

    [finish]Chapter17 Quiz

---
+++ Chapter17/Main10.java
@@ -0,0 +1,10 @@
+//17-4
+
+import java.io.IOException;
+
+public class Main10 {
+  public static void main(String[] args) throws IOException {
+    System.out.println("プログラムが起動しました");
+    throw new IOException();
+  }
+}
